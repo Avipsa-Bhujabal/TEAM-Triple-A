8b789d9ab29059779111fe02c860daf611bf2c2c
Avipsa-Bhujabal <85359975+Avipsa-Bhujabal@users.noreply.github.com>
2021-07-03 12:19:30 +0530

    Update TRIPLE-A_ASCI_2021.py

--- TRIPLE-A_ASCI_2021.py
+++ TRIPLE-A_ASCI_2021.py
@@ -7,11 +7,16 @@ import webbrowser
 import wikipedia 
 import wolframalpha
 import pyjokes
+from selenium import webdriver
+from tkinter import *
+from PIL import Image,ImageTk
+
+
 
 app_id = ('AUXY7Y-8PWEUGTRL6')
 user = wolframalpha.Client(app_id)
 
-print("STARTING, JERRY YOUR PERSONAL VIRTUAL ASSISTANT ")
+
 what = pyttsx3.init()
 r = s.Recognizer()
 
@@ -19,9 +24,6 @@ r = s.Recognizer()
 def speak(text):
     what.say(text)
     what.runAndWait()
-
-speak("STARTING, JERRY YOUR PERSONAL VIRTUAL ASSISTANT")
-
 #Taking name of user
 def take_name():
     speak("Who are you?")
@@ -35,7 +37,6 @@ def take_name():
         pass
     return name
 
-Name = take_name()
 # Wishing me 
 def greet():
     hr = datetime.datetime.now().hour
@@ -44,59 +45,112 @@ def greet():
     elif hr >=12 and hr<18 :
         speak("GOOD AFTERNOON " + Name)
     else :
-        speak("GOOD EVENING " + Name)
-greet()
+        speak("GOOD EVENING " + Name)   
 
-while True :
+#take command from microphone
+def order():
+    speak("How may I help you"+ Name)
+    try :
+        with s.Microphone() as sr :
+            print("Listening .... ")
+            voice = r.listen(sr)
+            speech = r.recognize_google(voice,language='en-in')
+            print(speech)
+    except Exception as e:
+        speak("Sorry! Say that again")
+        speech = None
+    return speech
 
-    #take command from microphone
-    def order():
-        speak("How may I help you"+ Name)
-        try :
-            with s.Microphone() as sr :
-                print("Listening .... ")
-                voice = r.listen(sr)
-                speech = r.recognize_google(voice)
-                print(speech)
-        except :
-                pass
-        return speech
+class gui() : #class for creating a GUI application
+    def __init__(s):
+        r = Tk()
+        r.title('Jerry , The Virtual Assistant')
+        r.geometry('1366x840')
+
+        img = ImageTk.PhotoImage(Image.open("F:/23de5967e32e56bb06115e30ac5ef4a1.jpg")) #Adding image in the application
+        panel = Label(r,image = img)
+        panel.pack(side='left',fill='y',expand='no')
+
+        s.ct = StringVar()
+        s.ut = StringVar()
+
+        s.ut.set("Click Run Jerry to Give Command")
+
+        uf = LabelFrame(r,text='User', font=('Black Chancery',20,'bold','italic'),fg='black',height=300,width=200)
+        uf.pack(fill='both',expand='yes')
+
+        user=Message(uf,textvariable=s.ut,bg='#001f03',fg='#e0d100')
+        user.config(font=('TIMES NEW ROMAN',30))
+        user.pack(fill='both',expand='yes')
+
+
+
+        cf = LabelFrame(r,text='Jerry', font=('Black Chancery',20,'bold','italic'),fg='black',height=300,width=200)
+        cf.pack(fill='both',expand='yes')
+
+        jerry=Message(cf,textvariable=s.ct,bg='#e0d100',fg='#001f03')
+        jerry.config(font=('TIMES NEW ROMAN',30,'bold'))
+        jerry.pack(fill='both',expand='yes')
+
+
+        b1 = Button(r,text='RUN JERRY',font=('ATHLETIC',15,'bold'),bg='#959499',fg='black',command=s.clicked)
+        b1.pack(fill='x',expand='yes')
+        b2 = Button(r,text='STOP ME',font=('ATHLETIC',15,'bold'),bg='#959499',fg='black',command=r.destroy)
+        b2.pack(fill='x',expand='yes')
+        s.ct.set("Hi, I am Jerry!!! How can I help you?")
+        r.mainloop()
     
-    speech = order()
-    #Execution of task 
-    if 'play' in speech :  #playing song on youtube
-        song = speech.replace('play',' ')
-        print('playing'+song)
-        speak('playing'+song)
-        pywhatkit.playonyt(song)
-    elif 'open Google' in speech : #opening google tab
-        google = speech.replace('open','')
-        print('Opening'+google)
-        speak('Opening'+google)
-        webbrowser.open('www.google.com')
-    elif 'who is ' in speech : #searching for famous person in wikipedia
-        wiki = speech.replace('who is ','')
-        know = wikipedia.summary(wiki,3)
-        print(know)
-        speak(know)
-    elif 'what is ' in speech : #searching any stuff in wikipedia
-        wiki = speech.replace('what is ','')
-        know = wikipedia.summary(wiki,3)
-        print(know)
-        speak(know)
-    elif 'joke' in speech :
-        joke = pyjokes.get_joke(language="en", category="all")
-        print(joke)
-        speak(joke)
-    elif 'stop' in speech : #breaking the loop of taking command 
-        speak('OK!BYE ')
-        speak('Have a nice day')
-        break
-    elif 'solve'or'weather'or'date'or'time' in speech: #to solve questions , get weather of a place and get date time
-        res = user.query(speech)
-        answer = next(res.results).text 
-        print(answer)
-        speak(answer)
+    def clicked(self): #clicking button to execute task
+        print('Working')
+        self.ut.set('Listening....')
+        speech=order()
+        self.ut.set(speech)
+        #Execution of task 
+        if 'play' in speech :  #playing song on youtube
+            song = speech.replace('play',' ')
+            self.ct.set('playing'+song)
+            speak('playing'+song)
+            pywhatkit.playonyt(song)
+        elif 'open Google' in speech : #opening google tab
+            google = speech.replace('open','')
+            self.ct.set('Opening'+google)
+            speak('Opening'+google)
+            webbrowser.open('www.google.com')
+        elif 'search for ' in speech : #searching from wikipedia
+            wiki = speech.replace('search for','')
+            know = wikipedia.summary(wiki,3)
+            self.ct.set(know)
+            speak(know)
+        elif 'joke' in speech : #To listen to joke
+            joke = pyjokes.get_joke(language="en", category="neutral")
+            print(joke)
+            speak(joke)
+        elif 'news' in speech:#To read news headlines
+            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
+            speak('Here are some headlines from the Times of India,Happy reading')
+        elif 'open silicon ERP' in speech:#Automating SILICON ERP
+            cpath="C:\\Users\mypc\chromedriver_win32\chromedriver.exe"
+            driver = webdriver.Chrome(executable_path=cpath)
+            silicon=speech.replace("open","")
+            self.ct.set("Opening "+silicon)
+            speak("Opening "+silicon)
+            driver.get('http://erp.silicon.ac.in/estcampus/')
+            username = driver.find_element_by_xpath('//*[@id="username"]')
+            username.send_keys('20bcsd08')
+            password = driver.find_element_by_xpath('//*[@id="password"]')
+            password.send_keys('2507July.')
+            button = driver.find_element_by_css_selector('button[type=submit]')
+            button.click()
+        elif 'solve'or'weather'or'date'or'time' or 'who is' or 'what is' in speech: #to solve questions , get weather of a place , get date time and other gk
+            res = user.query(speech)
+            answer = next(res.results).text 
+            self.ct.set(answer)
+            speak(answer)
+print("STARTING, JERRY YOUR PERSONAL VIRTUAL ASSISTANT ")
+speak("STARTING, JERRY YOUR PERSONAL VIRTUAL ASSISTANT")
+Name = take_name()
+greet()
+widget=gui()
   
         
 
